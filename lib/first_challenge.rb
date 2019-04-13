9ea41be6c6ae5bf3f329dd4c2628b0667eb3197f
Jasterix C <loune.calixte@gmail.com>
2019-04-13 02:29:11 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -20,7 +20,7 @@ def first_challenge
       value.delete_if {|favorite| favorite== "strawberry"
       }
       end
-    end
+    
   #remember to return your newly altered contacts hash!
   contacts
 end
